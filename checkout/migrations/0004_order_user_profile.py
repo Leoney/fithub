3778d4ddfcd6c64759d1a7529f82f15acc7421de
Lyubomila <lubomilag1@gmail.com>
2021-03-30 22:26:57 +0000

    Python black formatter changed file

--- checkout/migrations/0004_order_user_profile.py
+++ checkout/migrations/0004_order_user_profile.py
@@ -7,14 +7,20 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('profiles', '0001_initial'),
-        ('checkout', '0003_auto_20210321_1350'),
+        ("profiles", "0001_initial"),
+        ("checkout", "0003_auto_20210321_1350"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='order',
-            name='user_profile',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='orders', to='profiles.userprofile'),
+            model_name="order",
+            name="user_profile",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="orders",
+                to="profiles.userprofile",
+            ),
         ),
     ]
--- fithub/wsgi.py
+++ fithub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fithub.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fithub.settings")
 
 application = get_wsgi_application()
