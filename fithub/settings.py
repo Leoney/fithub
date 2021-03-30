32ea6473dfab1f88e725f13bf4e4484150406352
Lyubomila <lubomilag1@gmail.com>
2021-03-30 14:46:33 +0000

    Removed secret key and set debug

--- fithub/settings.py
+++ fithub/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=9_=!*b=kfx=74=!o6xsf2bqm^&s+6fvwd(exw4+%r(d9*$u&b'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fithub-app.herokuapp.com', 'localhost']
 
