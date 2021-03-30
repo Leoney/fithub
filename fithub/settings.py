86dc57a6c215e39632624d8e5043bfac77e0ff4d
Lyubomila <lubomilag1@gmail.com>
2021-03-30 14:05:08 +0000

    Added Procfile

--- fithub/settings.py
+++ fithub/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '=9_=!*b=kfx=74=!o6xsf2bqm^&s+6fvwd(exw4+%r(d9*$u&b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://fithub-app.herokuapp.com/', 'localhost']
 
 
 # Application definition
